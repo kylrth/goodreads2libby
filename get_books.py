8f4b54ce0a483de5c235dc5a47bc12a6c31ba25b
Kyle Roth <kylrth@gmail.com>
2023-02-16 16:17:26 +0000

    print the book authors as well

--- get_books.py
+++ get_books.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 import requests
 
 
-def yield_results(url: str):
+def get_books(url: str):
     page = 1
     count = 0
 
@@ -13,22 +13,27 @@ def yield_results(url: str):
         req = requests.get(url, params={"page": page})
         soup = BeautifulSoup(req.text, "html.parser")
 
-        rows = soup.select("td[class='field title']")
+        rows = soup.select("tr[class='bookalike review']")
         if len(rows) == 0:
             # When we go past the last page, GoodReads returns an empty page.
             break
 
         for row in rows:
-            item = row.select("a")[0]
+            title = row.select("td[class='field title']")[0].select("a")[0]
+            author = row.select("td[class='field author']")[0].select("a")[0]
 
             yield {
-                "title": item.attrs["title"],
-                "link": item.attrs["href"],
+                "author": author.string,
+                "title": title.attrs["title"],
+                "link": title.attrs["href"],
             }
             count += 1
 
         print(f"finished page {page} with {count} books so far", file=sys.stderr)
         page += 1
+
+
+fields = ["author", "title", "link"]
 
 
 if __name__ == "__main__":
@@ -37,7 +42,7 @@ if __name__ == "__main__":
     writer = csv.writer(sys.stdout)
 
     # header
-    writer.writerow(["title", "link"])
+    writer.writerow(fields)
 
-    for result in yield_results(url):
-        writer.writerow([result["title"], result["link"]])
+    for result in get_books(url):
+        writer.writerow([result[key] for key in fields])
